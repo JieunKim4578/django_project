16d23416874b89af02ba884d768b06ed400d316e
Jieun Kim <jieun.kim4758@gmail.com>
2021-05-08 16:22:52 +0900

    CBV Complete!

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Post 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
     template_name='blog/index.html'
     ordering='-pk'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name='blog/single_post_page.html'
