115342dd4d23379aa2907ce71a5b6a0d1e509d8a
Jieun Kim <jieun.kim4758@gmail.com>
2021-05-22 14:18:45 +0900

    create category page

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     #path('<int:pk>', views.single_post_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('category/<str:slug>/', views.category_page),
 ]
